f4b484ea570fcc1a16c621cb73fbac16ff97e8ff
rajae.ha <rajae.hamida@gmail.com>
2026-01-09 01:58:51 -0500

    Method Created

--- src/cydeo/test/rajae/Week1_OddOrEven.java
+++ src/cydeo/test/rajae/Week1_OddOrEven.java
@@ -2,15 +2,27 @@ package cydeo.test.rajae;
 
 public class Week1_OddOrEven {
 
-/*
-`Numbers -- odd & even
-Write a method which can identifies given number is even or odd
-EX:
-identify(5) -> "Odd"
-identify(6) -> "Even"
-* */
+    /*
+    `Numbers -- odd & even
+    Write a method which can identifies given number is even or odd
+    EX:
+    identify(5) -> "Odd"
+    identify(6) -> "Even"
+    * */
 public static void main(String[] args) {
 
+    System.out.println(identify(5));
+    System.out.println(identify(6));
+
+}
+
+public static String identify(int number){
+
+    if(number%2==0){
+        return "Even";
+    } else {
+        return "Odd";
+    }
 
 }
     
